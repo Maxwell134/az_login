47107b9dd86993ecee05ffca5c4f58e5b9368601
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2024-08-11 01:04:23 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -1,15 +1,14 @@
 import groovy.json.JsonSlurperClassic
 
-def az_login(credentialsID){
-
+def az_login(credentialsID) {
     try {
-        withCredentials([string(credentialsId: azure-credentials, variable: 'AZURE_CREDENTIALS')]) {
-
+        // Access the Jenkins credentials using the provided credentialsID
+        withCredentials([string(credentialsId: credentialsID, variable: 'AZURE_CREDENTIALS')]) {
             def jsonSlurper = new JsonSlurperClassic()
-            def credentialsJsonObj  = jsonSlurper.parseText(AZURE_CREDENTIALS)
+            def credentialsJsonObj = jsonSlurper.parseText(AZURE_CREDENTIALS)
 
-            // Ensure env is correctly initialized and converted to uppercase for consistency
-            def envUpperCase = env.toUpperCase()
+            // Ensure that `env.BRANCH_NAME` is correctly initialized for consistency
+            def envUpperCase = env.BRANCH_NAME?.toUpperCase() ?: 'DEV' // Default to 'DEV' if BRANCH_NAME is null
 
             def selectedCredentials = (envUpperCase.contains('UAT') || envUpperCase.contains('TEST') || envUpperCase.equals('PROD')) ?
                                         credentialsJsonObj['prod_credentials'] :
@@ -17,16 +16,14 @@ def az_login(credentialsID){
 
             def AZURE_USERNAME = selectedCredentials['username']
             def AZURE_PASSWORD = selectedCredentials['password']
-//             def AZURE_TENANT = selectedCredentials['tenant']
+            // def AZURE_TENANT = selectedCredentials['tenant'] // Uncomment if tenant is needed
 
             // Run the Azure login command
             sh "docker login -u ${AZURE_USERNAME} -p ${AZURE_PASSWORD}"
 
         }
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
         echo "Azure login failed: ${e.message}"
         error("Stopping pipeline due to failed Azure login.")
     }
-
 }
