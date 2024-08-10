89c390de393439f56700e8d076355556d8d092b4
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2024-08-11 02:21:33 +0530

    Update aksdeployer.groovy

--- aksdeployer.groovy
+++ aksdeployer.groovy
@@ -1,10 +1,11 @@
 // Load the pipeline configuration file
 def pipelineConfig = readJSON(file: 'pipeline.json')
-def environments = pipelineConfig.deployEnvironments
+def environments = pipelineConfig.aksDeploy.deployEnvironments
 
 // Retrieve the environment and credentials ID
 def env = env // Make sure to use the environment variable correctly
-def credentialsID = environments[env]?.CREDENTIALID ?: 'docker-credentials'
+// def credentialsID = environments[env]?.CREDENTIALID ?: 'docker-credentials'
+def credentialsID = environments.${environments}
 
 // Ensure that the deployer object is correctly initialized before calling docker_login
 deployer.docker_login(credentialsID, env)
