7da5ee267669f21c0291184ce9f9b3b1c60ede51
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2024-08-11 03:07:05 +0530

    Update aksdeployer.groovy

--- aksdeployer.groovy
+++ aksdeployer.groovy
@@ -1,15 +1,16 @@
 // Load the pipeline configuration file
 def pipelineConfig = readJSON(file: 'pipeline.json')
-def environments = pipelineConfig.aksDeploy.deployEnvironments
+def deployEnvironments = pipelineConfig.aksDeploy.deployEnvironments
 
 // Retrieve the environment and credentials ID
-def env = env // Make sure to use the environment variable correctly
-def credentialsID = environments[env]?.CREDENTIALID ?: 'azure-credentials'
-// def credentialsID = environments.${environments}
+// def env = env // Make sure to use the environment variable correctly
+// def credentialsID = environments[env]?.CREDENTIALID ?: 'azure-credentials'
+def deploygroup = deployEnvironments.${environments}
+def credentialsID =deploygroup.CREDENTIALID
 
 // Ensure that the deployer object is correctly initialized before calling docker_login
-deployer.docker_login(credentialsID, env)
+deployer.docker_login(credentialsID)
 
 // Add additional deployment logic as needed
-echo "Deploying to ${env} environment with credentials ID ${credentialsID}"
+echo "Deploying to ${environment} environment with credentials ID ${credentialsID}"
 // Example: sh "deploy_script.sh ${parsedJson}"
