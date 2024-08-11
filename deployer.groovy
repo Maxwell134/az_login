bd092129af0e212c8a67ea5bf358b5a9457b93e8
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2024-08-11 16:07:29 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -7,22 +7,22 @@ def docker_login(credentialsID) {
             def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
 
             // Determine Docker credentials based on the environment
-            def envUpperCase = env.toUpperCase()
+            // def envUpperCase = env.toUpperCase()
 
-            // Conditional logic to select credentials
-            def dockerCredID = (env.BRANCH_NAME?.toLowerCase().contains('prod') ||
-                    env.BRANCH_NAME?.toLowerCase().contains('dra') ||
-                    env.BRANCH_NAME?.toLowerCase().contains('uat') ||
-                    env.BRANCH_NAME?.toLowerCase().contains('test') ||
-                    env.BRANCH_NAME?.toLowerCase().contains('qa') ||
-                    env.BRANCH_NAME?.toLowerCase().equals('prd')) ?
-                    'prod_credentials' : 'non_prod_credentials'
+            // // Conditional logic to select credentials
+            // def dockerCredID = (env.BRANCH_NAME?.toLowerCase().contains('prod') ||
+            //         env.BRANCH_NAME?.toLowerCase().contains('dra') ||
+            //         env.BRANCH_NAME?.toLowerCase().contains('uat') ||
+            //         env.BRANCH_NAME?.toLowerCase().contains('test') ||
+            //         env.BRANCH_NAME?.toLowerCase().contains('qa') ||
+            //         env.BRANCH_NAME?.toLowerCase().equals('prd')) ?
+            //         'prod_credentials' : 'non_prod_credentials'
 
 
-            def selectedCredentials = credentialsJsonObj[dockerCredID]
+            // def selectedCredentials = credentialsJsonObj[dockerCredID]
 
-            def DOCKER_USERNAME = selectedCredentials['username']
-            def DOCKER_PASSWORD = selectedCredentials['password']
+            def DOCKER_USERNAME = credentialsJsonObj['username']
+            def DOCKER_PASSWORD = credentialsJsonObj['password']
 
             // Perform Docker login
             sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
