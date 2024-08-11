3c158c2ec9661e431115928ebcd7d0c06739dee2
Tridib Bas <89687126+Mintu4321@users.noreply.github.com>
2024-08-11 16:13:33 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -6,21 +6,6 @@ def docker_login(credentialsID) {
             def jsonSlurper = new JsonSlurperClassic()
             def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
 
-            // Determine Docker credentials based on the environment
-            // def envUpperCase = env.toUpperCase()
-
-            // // Conditional logic to select credentials
-            // def dockerCredID = (env.BRANCH_NAME?.toLowerCase().contains('prod') ||
-            //         env.BRANCH_NAME?.toLowerCase().contains('dra') ||
-            //         env.BRANCH_NAME?.toLowerCase().contains('uat') ||
-            //         env.BRANCH_NAME?.toLowerCase().contains('test') ||
-            //         env.BRANCH_NAME?.toLowerCase().contains('qa') ||
-            //         env.BRANCH_NAME?.toLowerCase().equals('prd')) ?
-            //         'prod_credentials' : 'non_prod_credentials'
-
-
-            // def selectedCredentials = credentialsJsonObj[dockerCredID]
-
             def DOCKER_USERNAME = credentialsJsonObj['username']
             def DOCKER_PASSWORD = credentialsJsonObj['password']
 
