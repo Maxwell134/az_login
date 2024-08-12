73bb0e551236c5bf7dcf554f5560314488e2589b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 01:16:51 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -11,9 +11,9 @@ def docker_login(credentialsID) {
             def DOCKER_PASSWORD = credentialsJsonObj['password']
 
             // Perform Docker login securely
-            sh """
-                echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
-            """
+          
+            sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+        
 
             // Confirm login success
             sh 'docker info'
