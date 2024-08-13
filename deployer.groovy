f156c46d667c17a1d7722a269f62f58578a55d2f
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 23:09:04 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -1,21 +1,20 @@
-import groovy.json.JsonSlurper
-
 def docker_login(credentialsID) {
     def result = [:]  // Create a map to store result status and message
 
     try {
         withCredentials([string(credentialsId: credentialsID, variable: 'DOCKER_CREDENTIALS')]) {
-            def jsonSlurper = new JsonSlurper()
+            def jsonSlurper = new groovy.json.JsonSlurper()
             def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
             def DOCKER_USERNAME = credentialsJsonObj['username']
             def DOCKER_PASSWORD = credentialsJsonObj['password']
 
-            docker_login_cmd = """
-            set +x
-            docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-            set -x
-            """
-            sh docker_login_cmd
+            println "DOCKER_USERNAME: ${DOCKER_USERNAME}"  // Debugging: Print username
+            println "DOCKER_PASSWORD: ${DOCKER_PASSWORD}"  // Debugging: Print password
+
+            // Docker login command using --password-stdin
+            sh '''
+            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+            '''
 
             result.success = true
             result.message = "Docker login successful."
@@ -27,36 +26,4 @@ def docker_login(credentialsID) {
 
     return result
 }
-return this 
-
-// import groovy.json.JsonSlurper
-
-// def docker_login(credentialsID) {
-//     def result = [:]  // Create a map to store result status and message
-
-//     try {
-//         withCredentials([string(credentialsId: credentialsID, variable: 'DOCKER_CREDENTIALS')]) {
-//             def jsonSlurper = new JsonSlurper()
-//             def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
-//             def DOCKER_USERNAME = credentialsJsonObj['username']
-//             def DOCKER_PASSWORD = credentialsJsonObj['password']
-//             println "DOCKER_USERNAME: ${DOCKER_USERNAME}"
-//             println "DOCKER_PASSWORD: ${DOCKER_PASSWORD}"
-
-
-//             // Docker login command
-//             sh 'docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"'
-
-//             result.success = true
-//             result.message = "Docker login successful."
-//         }
-//     } catch (Exception e) {
-//         result.success = false
-//         result.message = "Docker login failed: ${e.message}"
-//     }
-
-//     return result
-// }
-// return this
-
-
+return this
