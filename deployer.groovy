cd06efe7e464cc523e6b0defbb6c2d5d5458fda9
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 22:35:02 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -1,3 +1,34 @@
+// import groovy.json.JsonSlurper
+
+// def docker_login(credentialsID) {
+//     def result = [:]  // Create a map to store result status and message
+
+//     try {
+//         withCredentials([string(credentialsId: credentialsID, variable: 'DOCKER_CREDENTIALS')]) {
+//             def jsonSlurper = new JsonSlurper()
+//             def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
+//             def DOCKER_USERNAME = credentialsJsonObj['username']
+//             def DOCKER_PASSWORD = credentialsJsonObj['password']
+
+//             docker_login_cmd = """
+//             set +x
+//             docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+//             set -x
+//             """
+//             sh docker_login_cmd
+
+//             result.success = true
+//             result.message = "Docker login successful."
+//         }
+//     } catch (Exception e) {
+//         result.success = false
+//         result.message = "Docker login failed: ${e.message}"
+//     }
+
+//     return result
+// }
+// return this 
+
 import groovy.json.JsonSlurper
 
 def docker_login(credentialsID) {
@@ -10,12 +41,8 @@ def docker_login(credentialsID) {
             def DOCKER_USERNAME = credentialsJsonObj['username']
             def DOCKER_PASSWORD = credentialsJsonObj['password']
 
-            docker_login_cmd = """
-            set +x
-            docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-            set -x
-            """
-            sh docker_login_cmd
+            // Docker login command
+            sh 'docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"'
 
             result.success = true
             result.message = "Docker login successful."
@@ -27,4 +54,6 @@ def docker_login(credentialsID) {
 
     return result
 }
-return this 
+return this
+
+
