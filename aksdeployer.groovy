2f1d0c14f97cf403c3fd3dc65d401e65668eb849
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 17:19:41 +0530

    Update aksdeployer.groovy

--- aksdeployer.groovy
+++ aksdeployer.groovy
@@ -1,24 +1,3 @@
-// import groovy.json.JsonSlurper
-
-// def pipelineConfig = readJSON(file: 'pipeline.json')
-// def jsonContent = new JsonSlurper().parseText(pipelineConfig)
-
-//     // Load the environment-specific configuration
-// def deployEnvironments = jsonContent.aksDeploy.deployEnvironments
-// def deploygroup = deployEnvironments.dev
-// if (!deploygroup) {
-//     error "Environment '${environment}' not found in pipeline.json"
-// }
-// def credentialsID = deploygroup.'CREDENTIALID'
-
-//     // Ensure Docker login
-// docker_login(credentialsID)
-
-//     // Add your deployment logic here
-// echo "Deploying to ${environment} environment with credentials ID ${credentialsID}"
-
-//     // Example of deployment script
-//     // sh "deploy_script.sh ${environment}"
 
 import groovy.json.JsonSlurper
 
@@ -28,7 +7,7 @@ def deployToAks(environment) {
 
     // Load the environment-specific configuration
     def deployEnvironments = pipelineConfig.aksDeploy.deployEnvironments
-    def deploygroup = deployEnvironments.'${environment}'
+    def deploygroup = deployEnvironments[environment]
     if (!deploygroup) {
         error "Environment '${environment}' not found in pipeline.json"
     }
