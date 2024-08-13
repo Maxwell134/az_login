b6012a42c7f41c0dad5f7640afe67bef16eb53f9
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-14 02:54:02 +0530

    Update deployer.groovy

--- deployer.groovy
+++ deployer.groovy
@@ -12,11 +12,9 @@ def docker_login(credentialsID) {
 
             // Debugging: Print username and password (be careful with sensitive data)
             echo "DOCKER_USERNAME: ${DOCKER_USERNAME}"
-            // echo "Raw credentials: '${DOCKER_CREDENTIALS}'"
 
             // Docker login command using --password-stdin
-            
-           sh 'docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"'      
+            sh script: "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin", returnStdout: true
 
             result.success = true
             result.message = "Docker login successful."
@@ -28,37 +26,5 @@ def docker_login(credentialsID) {
 
     return result
 }
+
 return this
-// import groovy.json.JsonSlurper
-
-// def docker_login(credentialsID) {
-//     def result = [:]  // Create a map to store result status and message
-
-//     try {
-//         // Mock the JSON for testing
- 
-//         def jsonSlurper = new JsonSlurper()
-//         def credentialsJsonObj = jsonSlurper.parseText(DOCKER_CREDENTIALS)
-
-//         def DOCKER_USERNAME = credentialsJsonObj['username']
-//         def DOCKER_PASSWORD = credentialsJsonObj['password']
-
-        
-
-//         // Docker login command using --password-stdin
-//          sh 'docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"'      
-        
-        
-
-//         result.success = true
-//         result.message = "Docker login successful."
-//     } catch (Exception e) {
-//         result.success = false
-//         result.message = "Docker login failed: ${e.message}"
-//         echo result.message  // Log the failure message
-//     }
-
-//     return result
-// }
-
-// return this
