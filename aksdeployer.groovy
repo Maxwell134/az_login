95faef0cdd092aed00021d62dd0ac45152b58345
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 21:04:01 +0530

    Update aksdeployer.groovy

--- aksdeployer.groovy
+++ aksdeployer.groovy
@@ -1,29 +1,33 @@
-import groovy.json.JsonSlurper
-
-def deployToAks(environment) {
-    // Load the pipeline configuration
-    def pipelineConfig = readJSON(file: 'pipeline.json')
-
+def call(env, pipelineConfig) {
     // Load the environment-specific configuration
     def deployEnvironments = pipelineConfig.aksDeploy.deployEnvironments
-    def deploygroup = deployEnvironments[environment]
+    def deploygroup = deployEnvironments[env]
+
     if (!deploygroup) {
-        error "Environment '${environment}' not found in pipeline.json"
+        error "Environment '${env}' not found in the pipeline configuration."
     }
 
     def credentialsID = deploygroup.CREDENTIALID
 
-    // Load the deployer script
-    def deployer = load 'deployer.groovy'
+    // Load the dockerUtils.groovy script
+    def dockerUtils = load 'dockerUtils.groovy'
 
-    // Ensure Docker login
-    deployer.docker_login(credentialsID)
+    // Call the docker_login method and get the result
+    def loginResult = deployer.docker_login(credentialsID)
+
+    // Check the result of Docker login
+    if (loginResult.success) {
+        echo loginResult.message
+    } else {
+        echo loginResult.message
+        error("Stopping pipeline due to failed Docker login.")
+    }
 
     // Add your deployment logic here
-    echo "Deploying to ${environment} environment with credentials ID ${credentialsID}"
+    echo "Deploying to ${env} environment with credentials ID ${credentialsID}"
 
     // Example of deployment script
-    // sh "deploy_script.sh ${environment}"
+    // sh "deploy_script.sh ${env}"
 }
 
 return this
