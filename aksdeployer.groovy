37a8d4979deda81c49d000d2bf812ab19dcc2305
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-08-13 21:05:05 +0530

    Update aksdeployer.groovy

--- aksdeployer.groovy
+++ aksdeployer.groovy
@@ -13,7 +13,7 @@ def call(env, pipelineConfig) {
     def dockerUtils = load 'deployer.groovy'
 
     // Call the docker_login method and get the result
-    def loginResult = deployer.docker_login(credentialsID)
+    def loginResult = dockerUtils.docker_login(credentialsID)
 
     // Check the result of Docker login
     if (loginResult.success) {
